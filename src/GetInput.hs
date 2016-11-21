859a3709a1b3d7f799784b8210f5f47c0aca104c
Shaun <shaun.lee@gmail.com>
2016-11-20 23:33:30 -0800

    Delete GetInput.hs

--- src/GetInput.hs
+++
@@ -1,9 +0,0 @@
-{-# LANGUAGE OverloadedStrings #-}
-
-module GetInput where
-
-import           Control.Lens
-import           Data.Aeson      (Value)
-import           Data.Aeson.Lens (key, _String)
-import           Data.Map        as Map
-import           Network.Wreq
